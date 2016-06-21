04f0037f91a67b9328aa2113cc3cfe11064a91dd
Ivansamara <ivansamara109@mail.ru>
2016-06-22 00:48:59 +0400

    Заполнил базу

--- src/main/java/ru/home/appMain/AppMain.java
+++ src/main/java/ru/home/appMain/AppMain.java
@@ -7,6 +7,7 @@ import ru.home.utils.PasswordEncoder;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -19,8 +20,9 @@ public class AppMain {
 
     public static void main(String[] args) {
 //ПЕРЕД ЗАПУСКОМ УБЕДИТЕСЬ ЧТО МАКСИМАЛЬНАЯ ДЛИНА ПАРОЛЯ В БАЗЕ НЕ МЕНЕЕ 32 СИМВОЛОВ
-/*------------- Добавление записей в базу
         EntityManager em = DbHelper.getEm();
+
+/*------------- Добавление записей в базу
         em.getTransaction().begin();
         Role roleRes = new Role(RESIDENT, "Resident");
         Role roleProv = new Role(PROVIDER_EMP,"Provider employee");
@@ -72,8 +74,61 @@ public class AppMain {
         em.merge(hoasEmp);
 
         em.getTransaction().commit();
-        em.close();
-*/
+        */
+        em.getTransaction().begin();
+        int providerId = DbHelper.getNewId(Provider.class);
+        Provider provider = new Provider(providerId++,"Dom.ru","Москва");
+        Provider provider2 = new Provider(providerId++,"Водоканал","Самара");
+        Provider provider3 = new Provider(providerId++,"Газовая компания","Самара");
+        Provider provider4 = new Provider(providerId++,"Диван тв","Казань");
+        Provider provider5 = new Provider(providerId++,"Грузоперевозка","Хабаровск");
+        em.merge(provider);
+        em.merge(provider2);
+        em.merge(provider3);
+        em.merge(provider4);
+        em.merge(provider5);
+        em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        int serviceId = DbHelper.getNewId(Service.class);
+        Service service = new Service(serviceId++,"Интернет",1,false);
+        Service service2 = new Service(serviceId++,"Водоснабжение",2,true);
+        Service service3 = new Service(serviceId++,"Газ",3,true);
+        Service service4 = new Service(serviceId++,"Кабельное ТВ",4,false);
+        Service service5 = new Service(serviceId++,"Перевоз мебели",5,false);
+        em.merge(service);
+        em.merge(service2);
+        em.merge(service3);
+        em.merge(service4);
+        em.merge(service5);
+        em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        int abonentId = DbHelper.getNewId(Abonent.class);
+        Abonent abonent = new Abonent(abonentId++,1,111111111,4);
+        Abonent abonent2 = new Abonent(abonentId++,2,222222222,4);
+        Abonent abonent3 = new Abonent(abonentId++,3,333333333,4);
+        Abonent abonent4 = new Abonent(abonentId++,4,444444444,4);
+        Abonent abonent5 = new Abonent(abonentId++,5,555555555,4);
+        em.merge(abonent);
+        em.merge(abonent2);
+        em.merge(abonent3);
+        em.merge(abonent4);
+        em.merge(abonent5);
+        em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        int chargeId = DbHelper.getNewId(Charge.class);
+        Charge charge = new Charge(chargeId++,1,600,new Date("01.02.2016"),new Date("01.03.2016"),true);
+        Charge charge2 = new Charge(chargeId++,2,100,new Date("01.02.2016"),new Date("01.03.2016"),false);
+        Charge charge3 = new Charge(chargeId++,3,600,new Date("01.01.2016"),new Date("01.03.2016"),true);
+        Charge charge4 = new Charge(chargeId++,4,600,new Date("01.03.2016"),new Date("01.05.2016"),false);
+        Charge charge5 = new Charge(chargeId++,5,600,new Date("01.01.2016"),new Date("01.04.2016"),true);
+        em.getTransaction().commit();
+
+        
+
+
 
 
 /*-------------- Вытаскивание из базы по первичному ключу.
